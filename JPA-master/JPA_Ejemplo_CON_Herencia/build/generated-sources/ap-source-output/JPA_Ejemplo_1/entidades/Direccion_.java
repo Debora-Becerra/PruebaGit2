534780ae088b662c8c1a5b8a9d073f5f96163202
Debora Becerra <debo-bc@hotmail.com>
2023-01-25 20:32:37 -0300

    archivos jpa

---
+++ JPA-master/JPA_Ejemplo_CON_Herencia/build/generated-sources/ap-source-output/JPA_Ejemplo_1/entidades/Direccion_.java
@@ -0,0 +1,15 @@
+package JPA_Ejemplo_1.entidades;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-01-18T19:38:04")
+@StaticMetamodel(Direccion.class)
+public class Direccion_ { 
+
+    public static volatile SingularAttribute<Direccion, String> id;
+    public static volatile SingularAttribute<Direccion, String> provincia;
+    public static volatile SingularAttribute<Direccion, String> pais;
+
+}
